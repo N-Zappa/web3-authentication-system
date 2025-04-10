bd6fa3345f71f9cc4b34a5e7b08f642788315c50
N-Zappa <ilyaokulov5@gmail.com>
2025-04-11 01:05:28 +0300

    configured swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './config/config';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+  app.enableCors();
+  app.use(helmet());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
